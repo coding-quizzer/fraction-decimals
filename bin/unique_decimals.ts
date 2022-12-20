c4840393c9cf50ae4fb4fe96913e51cc011ca983
William Gadd <wgadd@sasktel.net>
2022-12-20 17:46:01 -0600

    Show numerators for repeated decimals

--- bin/unique_decimals.ts
+++ bin/unique_decimals.ts
@@ -6,6 +6,15 @@ const denominator: string = process.argv[2];
 const decimalList: RepeatingDecimalObject[] = getUniqueDecimals(Number(denominator));
 
 for (const decimal of decimalList) {
+  console.log(
+    `Numerator: ${
+      decimal.nonRepeatingDigits
+        ? decimal.nonRepeatingDigits[0].baseNumerator
+        : decimal.repeatingDigits
+        ? decimal.repeatingDigits[0].baseNumerator
+        : null
+    }`
+  );
   console.log(convertDecimalObjectToString(decimal));
   console.log();
 }

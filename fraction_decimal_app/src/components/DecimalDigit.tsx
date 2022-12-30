3214b68f461e35258e06168d658a3da82454f190
William Gadd <wgadd@sasktel.net>
2022-12-30 10:55:36 -0600

    Make arrow for popper and center popper and arrow correctly around the number

--- fraction_decimal_app/src/components/DecimalDigit.tsx
+++ fraction_decimal_app/src/components/DecimalDigit.tsx
@@ -1,3 +1,4 @@
+import { auto } from "@popperjs/core";
 import React, { useState } from "react";
 import { usePopper } from "react-popper";
 import { Fraction } from "../../../fractionDecimalFunctions";
@@ -11,10 +12,26 @@ export default function DecimalDigit(props: DecimalDigitProps) {
   const [popperIsVisible, setPopperIsVisible] = useState<boolean | null>(null);
   const [referenceElement, setReferenceElement] = useState<HTMLDivElement | null>(null);
   const [popperElement, setPopperElement] = useState<HTMLDivElement | null>(null);
-  const { styles, attributes } = usePopper(referenceElement, popperElement);
+  const [arrowElement, setArrowElement] = useState<HTMLDivElement | null>(null);
+  const { styles, attributes } = usePopper(referenceElement, popperElement, {
+    placement: "bottom-start",
+    modifiers: [
+      {
+        name: "offset",
+        options: {
+          offset: [-8, 13],
+        },
+      },
+      {
+        name: "arrow",
+        options: {
+          element: arrowElement,
+        },
+      },
+    ],
+  });
 
   const { fraction } = props;
-  console.log(attributes);
 
   return (
     <>
@@ -31,7 +48,15 @@ export default function DecimalDigit(props: DecimalDigitProps) {
         ref={setPopperElement}
         style={styles.popper}
         {...(attributes.popper, { "data-show": popperIsVisible })}
-      >{`${fraction[0]} / ${fraction[1]}`}</div>
+      >
+        {`${fraction[0]} / ${fraction[1]}`}
+        <div
+          data-popper-arrow
+          className="decimal-digit--popper-arrow"
+          ref={setArrowElement}
+          style={styles.arrow}
+        ></div>
+      </div>
     </>
   );
 }

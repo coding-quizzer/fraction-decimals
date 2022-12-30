54c4679a88163a98166e89fadbc4174e8cc30188
William Gadd <wgadd@sasktel.net>
2022-12-29 23:06:35 -0600

    Install react popper-js library and display mock popup when a decimal digit is hovered over by the mouse

--- fraction_decimal_app/src/components/DecimalDigit.tsx
+++ fraction_decimal_app/src/components/DecimalDigit.tsx
@@ -1,11 +1,33 @@
-import "./DecimalDigit.scss"
-type DecimalDigitProps ={
+import React, { useState } from "react";
+import { usePopper } from "react-popper";
+import "./DecimalDigit.scss";
+type DecimalDigitProps = {
   children: React.ReactNode;
-}
+};
+
 export default function DecimalDigit(props: DecimalDigitProps) {
+  const [popperIsVisible, setPopperIsVisible] = useState<boolean | null>(null);
+  const [referenceElement, setReferenceElement] = useState<HTMLDivElement | null>(null);
+  const [popperElement, setPopperElement] = useState<HTMLDivElement | null>(null);
+  const { styles, attributes } = usePopper(referenceElement, popperElement);
+  console.log(attributes);
+
   return (
-      <div className="decimal-digit">
+    <>
+      <div
+        className="decimal-digit"
+        ref={setReferenceElement}
+        onMouseOver={() => setPopperIsVisible(true)}
+        onMouseLeave={() => setPopperIsVisible(null)}
+      >
         {props.children}
       </div>
-  )
+      <div
+        className="decimal-digit--popper"
+        ref={setPopperElement}
+        style={styles.popper}
+        {...(attributes.popper, { "data-show": popperIsVisible })}
+      ></div>
+    </>
+  );
 }

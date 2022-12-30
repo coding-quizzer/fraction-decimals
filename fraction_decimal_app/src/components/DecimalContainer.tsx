4370233338facdbe3558b412bfe36a4d8d9cfae9
William Gadd <wgadd@sasktel.net>
2022-12-29 18:28:11 -0600

    Create decimal container to store decimal digits and any extra information or controls around them

---
+++ fraction_decimal_app/src/components/DecimalContainer.tsx
@@ -0,0 +1,15 @@
+import { Fraction } from "../../../fractionDecimalFunctions";
+import { convertFractionToRepeatingDecimal } from "../../../fractionDecimalFunctions";
+import "./DecimalContainer.scss";
+import DigitList from "./DigitList";
+
+type DecimalContainerProps = {
+  fraction: Fraction;
+};
+export function DecimalContainer(props: DecimalContainerProps) {
+  return (
+    <div className="decimal-container">
+      <DigitList digits={convertFractionToRepeatingDecimal(props.fraction)} />
+    </div>
+  );
+}
---
+++ fraction_decimal_app/src/stories/DecimalContainer.stories.tsx
@@ -0,0 +1,6 @@
+import { DecimalContainer } from "../components/DecimalContainer";
+export default {
+  title: "DecimalContainer",
+  component: DecimalContainer,
+};
+export const Default = () => <DecimalContainer fraction={[1, 1001]} />;

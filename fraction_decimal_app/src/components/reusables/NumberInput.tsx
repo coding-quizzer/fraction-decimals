7b2647578f3667e69004abdcf59a8a405167ff59
William Gadd <wgadd@sasktel.net>
2023-01-04 12:18:04 -0600

    Create a new component for number inputs

---
+++ fraction_decimal_app/src/components/reusables/NumberInput.tsx
@@ -0,0 +1,22 @@
+type NumberInputProps = {
+  name: string;
+  value: string;
+  setValue: (value?: string) => void;
+};
+
+export default function NumberInput(props: NumberInputProps) {
+  const { name, value, setValue } = props;
+  return (
+    <input
+      name={name}
+      value={value}
+      onChange={(e) => {
+        (!e.target.value || Number(e.target.value)) && setValue(e.target.value);
+      }}
+      type="number"
+      className="number-input"
+      style={{ width: value ? `${value.length * 0.6 + 1}em` : "1.6em" }}
+      min={1}
+    />
+  );
+}

91e93b443bec7b4d0e754565ff9c2ccc65e7d319
William Gadd <wgadd@sasktel.net>
2023-01-09 13:10:10 -0600

    Use React.forwardRef function to forward refs applied to the number input component

--- fraction_decimal_app/src/components/reusables/NumberInput.tsx
+++ fraction_decimal_app/src/components/reusables/NumberInput.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import "./NumberInput.scss";
 type NumberInputProps = {
   name: string;
@@ -5,19 +6,26 @@ type NumberInputProps = {
   setValue: (value: number | null) => void;
 };
 
-export default function NumberInput(props: NumberInputProps) {
-  const { name, value, setValue } = props;
-  return (
-    <input
-      name={name}
-      value={value ? value : ""}
-      onChange={(e) => {
-        (!e.target.value || Number(e.target.value)) && setValue(Number(e.target.value));
-      }}
-      type="number"
-      className="number-input"
-      style={{ width: value ? `${String(value).length * 0.6 + 1.4}em` : "2em" }}
-      min={1}
-    />
-  );
-}
+const NumberInput = React.forwardRef<HTMLInputElement, NumberInputProps>(
+  function NumberInput(props, ref) {
+    const { name, value, setValue } = props;
+    return (
+      <input
+        name={name}
+        value={value ? value : ""}
+        onChange={(e) => {
+          (!e.target.value || Number(e.target.value)) && setValue(Number(e.target.value));
+        }}
+        type="number"
+        className="number-input"
+        style={{ width: value ? `${String(value).length * 0.6 + 1.4}em` : "2em" }}
+        min={1}
+        ref={ref}
+      />
+    );
+  }
+);
+
+NumberInput.displayName = "Number Input";
+
+export default NumberInput;

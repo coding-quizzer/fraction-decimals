3199b483e69b71b5d8f8ee8a033ed9950614bb00
William Gadd <wgadd@sasktel.net>
2023-01-09 10:57:41 -0600

    Allow user to edit the numerator and denominator in the input container header

--- fraction_decimal_app/src/components/DecimalContainer/index.tsx
+++ fraction_decimal_app/src/components/DecimalContainer/index.tsx
@@ -10,8 +10,17 @@ import useDebounce from "../hooks/useDebounce";
 type DecimalContainerProps = {
   fraction: Fraction;
 };
+
 export default function DecimalContainer(props: DecimalContainerProps) {
-  const [numerator, denominator] = props.fraction;
+  const [calculatedNumerator, setCalculatedNumerator] = useState(props.fraction[0]);
+  const [calculatedDenominator, setCalculatedDenominator] = useState(props.fraction[1]);
+
+  const [inputNumerator, setInputNumerator] = useState(props.fraction[0]);
+  const [inputDenominator, setInputDenominator] = useState(props.fraction[1]);
+
+  useDebounce(() => setCalculatedNumerator(inputNumerator));
+  useDebounce(() => setCalculatedDenominator(inputDenominator));
+
   const [digitLimit, setDigitLimit] = useState("8");
   const [tempDigitLimit, setTempDigitLimit] = useState("8");
   useDebounce(() => setDigitLimit(tempDigitLimit));
@@ -19,9 +28,17 @@ export default function DecimalContainer(props: DecimalContainerProps) {
     <div className="decimal-container">
       <header>
         <div className="decimal-container--label">
-          <NumberContainer>{numerator}</NumberContainer>
+          <NumberInput
+            name="numerator-container"
+            value={String(inputNumerator)}
+            setValue={(numerator: string) => setInputNumerator(Number(numerator))}
+          />
           <section>/</section>
-          <NumberContainer>{denominator}</NumberContainer>
+          <NumberInput
+            value={String(inputDenominator)}
+            name="denominator-container"
+            setValue={(denominator: string) => setInputDenominator(Number(denominator))}
+          />
         </div>
         <div className="decimal-container--limit">
           <label htmlFor={"digit-limit"}>Max Digits</label>
@@ -33,8 +50,11 @@ export default function DecimalContainer(props: DecimalContainerProps) {
         </div>
       </header>
       <DigitList
-        digits={convertFractionToRepeatingDecimal(props.fraction)}
-        denominator={denominator}
+        digits={convertFractionToRepeatingDecimal([
+          calculatedNumerator,
+          calculatedDenominator,
+        ])}
+        denominator={calculatedDenominator}
         limit={Number(digitLimit)}
       />
     </div>
